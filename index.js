6fdbfed4e519b682b17e01f63a8e005635d6521e
jacopofar <jacopo1.farina@gmail.com>
2015-07-20 17:45:41 +0200

    updated to not use the (apparently broken) node bzip and removed unused

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
 var fs = require('fs');
 var stream = require('stream');
-var Fiber = require('fibers');
-var Bunzip = require('seek-bzip');
+var split = require('split');
 
-/** Use node-fibers to convert our synchronous Stream interface to the
-  * standard node asynchronous interface. */
-var BunzipStream = function() {
-    var trans = this;
-    stream.Transform.call(trans); // initialize superclass.
-    this._fiber = new Fiber(function() {
-        var buffer = [], pos = 0;
-        var inputStream = new Bunzip.Stream();
-        inputStream.readByte = function() {
-            if (pos >= buffer.length) {
-                buffer = Fiber.yield(); pos = 0;
-            }
-            return buffer[pos++];
-        };
-        var outputStream = new Bunzip.Stream();
-        outputStream.writeByte = function(_byte) {
-            this.write(new Buffer([_byte]),0,1);
-        };
-        outputStream.write = function(buffer, bufOffset, length) {
-            if (bufOffset !== 0 || length !== buffer.length) {
-                buffer = buffer.slice(bufOffset, bufOffset + length);
-            }
-            trans.push(buffer);
-        };
-        Bunzip.decode(inputStream, outputStream);
-    });
-    this._fiber.run();
-};
-BunzipStream.prototype = Object.create(stream.Transform.prototype);
-BunzipStream.prototype._transform = function(chunk, encoding, callback) {
-    this._fiber.run(chunk);
-    callback();
-};
 
-var outputStream = fs.createReadStream('/data/RC_2015-05.bz2').pipe(new BunzipStream());
-
-outputStream.on('readable', function() {
-              var b = outputStream.read(), i;
-              console.log(b.toString());
-          }).on('end', function() {
-              assert.equal(pos, data.length);
-              assert.equal(data.toString('hex'), referenceData.toString('hex'));
-              callback();
-          });
+//var outputStream = fs.createReadStream('/data/RC_2015-05.bz2').pipe(bz2())
+var outputStream = process.stdin.pipe(split(JSON.parse));
+var k = 0;
+var pruningThreshold = 1;
+var pruningCount = 0;
+var wordCount = {};
+var start = new Date().getTime();
+outputStream.on('data', function(reddit_post) {
+  k++;
+  reddit_post.body.toLowerCase().split(' ').forEach(function(token){
+  //ignore everything which is not clearly a word
+  //we use Latin1 letters, this excludes a lot of alphabets, but there's not an easy way to manage them, Reddit is mostly in English and for Chinese and Japanese we'd need a tokenizer in any case
+  if(!(/^[a-z\u00C0-\u00ff]+$/g).test(token)){
+    return;
+  }
+  if(wordCount[token])
+    wordCount[token]++;
+  else
+    wordCount[token] = 1;
+  });
+  if(k % 5000 === 0){
+    var elapsed = (new Date().getTime()-start)/1000;
+    console.log("processed "+k+" posts, current dictionary of "+Object.keys(wordCount).length+" distinct words. "+(k/elapsed)+" posts per second");
+    if(Object.keys(wordCount).length > 120000){
+      console.log("post "+k+" found "+Object.keys(wordCount).length +" distinct words, pruning the ones appearing less than "+pruningThreshold+" times...");
+      for(word in wordCount){
+        //console.log("the word "+word+ " appears "+wordCount[word]+ "times ";
+        if(wordCount[word] <= pruningThreshold)
+          delete wordCount[word];
+      }
+      pruningCount++;
+      console.log("after pruning number #"+pruningCount+" there are "+Object.keys(wordCount).length +" distinct words");
+    
+      if(pruningCount % 10 === 0){
+        fs.writeFile("count.txt",JSON.stringify(wordCount),function(err){
+        if(err){console.log("error writing file!");process.exit(1);}
+        });
+      }
+      if(Object.keys(wordCount).length>110000)
+        pruningThreshold++;
+    }
+  }
+}).on('end', function() {
+  console.log("END!!");
+  process.exit(0);          
+});
